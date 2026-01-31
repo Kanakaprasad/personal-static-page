4d84a2455a311757bd9282e84ee01203f87d7cc5
Kanaka <kanakaprasad.2001@gmail.com>
2026-01-31 13:07:46 +0530

    Pushing the latest code

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const music = document.getElementById("bgMusic");
 const muteBtn = document.getElementById("muteBtn");
 let muted = false;
 
-/* 🎵 Music per stage */
+/* Music map */
 const musicMap = {
   hero: "./music/intro.mp3",
   1: "./music/intro.mp3",
@@ -23,42 +23,13 @@ const musicMap = {
   6: "./music/ending.mp3"
 };
 
-/* ❤️ Hearts */
+/* Hearts */
 lottie.loadAnimation({
   container: document.getElementById("lottie-hearts"),
   renderer: "svg",
   loop: true,
   autoplay: true,
   path: "https://assets10.lottiefiles.com/packages/lf20_jppxqf.json"
-});
-
-/* 📸 Photos (Cloudinary) */
-const photos = [
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758489/Sanju-1_x6t8eh.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758489/SanKP-9_whwphg.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758484/SanKP-5_dbjx0a.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758483/Sanju-2_kz1yqa.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758482/Sanju-4_r0xjpd.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758480/Sanju-7_iksay0.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758479/Sanju-8_zlcbhy.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758476/SanKP-2_yqisgv.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758474/Sanju-3_noksgw.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758469/SanKP-3_xa8ckt.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758467/SanKP-10_szafd3.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758467/SanKP-4_r7eflo.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758466/Sanju-6_mweqgu.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758463/SanKP-1_yb6czj.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758463/SanKP-6_ilbntr.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758462/SanKP-8_dqvuie.jpg",
-  "https://res.cloudinary.com/dlsp49kl5/image/upload/v1769758458/SanKP-7_zn4n5s.jpg"
-];
-
-const cardsContainer = document.getElementById("photoCards");
-photos.forEach(src => {
-  const card = document.createElement("div");
-  card.className = "photo-card";
-  card.innerHTML = `<img src="${src}" loading="lazy">`;
-  cardsContainer.appendChild(card);
 });
 
 /* Login */
@@ -89,7 +60,14 @@ function nextStep() {
   playMusic(currentStep);
 }
 
-/* Music control */
+function prevStep() {
+  document.getElementById(`step${currentStep}`).classList.remove("active");
+  currentStep--;
+  document.getElementById(`step${currentStep}`).classList.add("active");
+  playMusic(currentStep);
+}
+
+/* Music */
 function playMusic(key) {
   if (muted) return;
   music.src = musicMap[key];
